c9b4ae405c259c9ae6c3eedb5052820f109c888a
Viktor Gilko <vgilkojob@gmail.com>
2022-09-08 23:24:08 +0300

    Solved first task on java. Optimal

--- day_1/Task1.java
+++ day_1/Task1.java
@@ -17,8 +17,21 @@ public class Task1 {
         }
     }
 
+    static class SolutionOptimal extends Solution {
+        public int[] runningSum(int[] nums) {
+            int[] result = new int[nums.length];
+
+            result[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                result[i] = result[i-1] + nums[i];
+            }
+
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution solution = new SolutionOptimal();
 
         int[] test = {3,1,2,10,1};
 
