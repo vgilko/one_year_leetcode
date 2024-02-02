217e777eb93b180f56e8ec71f74dda7eba86f4ac
vgilko <v.gilko@mkskom.com>
2024-02-02 18:59:49 +0300

    70. Climbing Sairs

--- day_1/Task1.java
+++ day_1/Task1.java
@@ -1,40 +1,29 @@
-import java.util.Arrays;
-
 public class Task1 {
     static class Solution {
-        public int[] runningSum(int[] nums) {
-            int[] result = new int[nums.length];
 
-            for (int idx = 0; idx < nums.length; idx++) {
-                int j = 0;
-                do {
-                    result[idx] += nums[j];
-
-                } while (j++ < idx);
+        public int climbStairs(int n) {
+            if (n <= 2) {
+                return n;
             }
 
-            return result;
-        }
-    }
+            int current = 1;
+            int prev = 1;
 
-    static class SolutionOptimal extends Solution {
-        public int[] runningSum(int[] nums) {
-            int[] result = new int[nums.length];
-
-            result[0] = nums[0];
-            for (int i = 1; i < nums.length; i++) {
-                result[i] = result[i-1] + nums[i];
+            for (int i = 3; i < n + 1; ++i) {
+                current += prev;
+                prev = current - prev;
             }
 
-            return result;
+            return current + prev;
         }
     }
 
     public static void main(String[] args) {
-        Solution solution = new SolutionOptimal();
+        Solution solution = new Solution();
 
-        int[] test = {3,1,2,10,1};
-
-        System.out.println(Arrays.toString(solution.runningSum(test)));
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println("\n" + solution.climbStairs(3));
+        System.out.println("\n" + solution.climbStairs(44));
     }
 }
