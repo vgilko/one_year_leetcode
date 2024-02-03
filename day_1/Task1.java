5bc92d73b2ab493a381607079b0897e8561326c6
vgilko <v.gilko@mkskom.com>
2024-02-03 16:49:25 +0300

    121. Best Time to Buy and Sell Stock TL solution

--- day_1/Task1.java
+++ day_1/Task1.java
@@ -1,29 +1,31 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class Task1 {
     static class Solution {
-        public List<Integer> getRow(int rowIndex) {
-            Integer[] row = new Integer[rowIndex + 1];
+        public int maxProfit(int[] prices) {
+            if (prices.length < 2) {
+                return 0;
+            }
 
-            Arrays.fill(row, 0);
-            row[0] = 1;
+            int maxPrice = 0;
 
-            for (int i = 1; i < rowIndex + 1; ++i) {
-                for (int j = i; j >= 1; --j) {
-                    row[j] += row[j - 1];
+            for (int i = 0; i < prices.length; ++i) {
+                for (int j = i; j < prices.length; ++j) {
+                    int difference = prices[j] - prices[i];
+                    if (difference > maxPrice) {
+                        maxPrice = difference;
+                    }
                 }
             }
 
-            return Arrays.asList(row);
+            return maxPrice;
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.getRow(3));
-        System.out.println(solution.getRow(5));
+//        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+//        System.out.println(solution.getRow(5));
 //
 //        long start = System.currentTimeMillis();
 //        System.out.println(solution.generate(1000));
